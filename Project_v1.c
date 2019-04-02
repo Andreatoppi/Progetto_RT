30e2b47c44994eff0af5582174999089df28bbc6
Andreatoppi <and.toppi@gmail.com>
2019-04-02 12:20:09 +0200

    Funzionante con N tank, ogni tank ha il suo bottone

--- Project_v1.c
+++ Project_v1.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <math.h>
 
+#define N           3  //number of tank
 #define XWIN        1000 // window x resolution
 #define YWIN        480 // window y resolution
 #define BKG         0   // background color
@@ -22,20 +23,27 @@
 #define MINLEVEL    0   // min quantity of liquid in tank
 #define HTANK       300 // tank hight
 #define WTANK       100 // tank width
-#define X1TANK      200 // position of tank
+#define X1TANK       200 // position of tank
 #define Y1TANK      100 // position of tank
+#define R           25  // button radious 
 
-int desired_level = 150;
+int desired_level = 100;
 
 struct tank_t {     //tank data structure
-    pthread_mutex_t mutex[2];
-    pthread_cond_t  C_f[2], C_t[2];
+    pthread_mutex_t mutex[N];
+    pthread_cond_t  C_f[N], C_t[N];
     int level;
     int x1, y1, x2, y2, h, w, color;
     int sensor, xsensor;
     int tnum;
     bool    tap;
-}tank[2];
+}tank[N];
+
+struct button {
+    int x, y, r;    //circle coordinates
+    int textcolor, bkgcolor;    //text and background color
+    char message;
+}button[N];
 
 void init_tank_t(struct tank_t *t, int i){        //tank inizialization
 
@@ -46,14 +54,23 @@ void init_tank_t(struct tank_t *t, int i){        //tank inizialization
     t->tap  = FALSE;
     t->x1 = X1TANK * i;
     t->y1 = Y1TANK;
-    t->x2 = X1TANK * i + WTANK;
+    t->x2 = (X1TANK * i) + WTANK;
     t->y2 = (Y1TANK + HTANK);
     t->h = HTANK;
     t->w = WTANK;
     t->color = WHITE;
     t->sensor = SMIN;
-    t->xsensor = (X1TANK * i) +50;
+    t->xsensor = (X1TANK * i) + (WTANK / 2);
     t->tnum = i;
+}
+
+void init_button(struct button *b, struct tank_t *t){
+    b->x = t->xsensor;
+    b->y = t->y1 - 50;
+    b->r = R;
+    b->textcolor = BKG;
+    b->bkgcolor = WHITE;
+    // b->message = "Open";
 }
 
 //init of allegro
@@ -70,9 +87,9 @@ void create_tank(struct tank_t *t){
     rectfill(screen, t->x1, t->y1, t->x2, t->y2, t->color);
 }
 
-void create_button(){
-    circle(screen, 100, 200, 50, 255);
-    textout_centre_ex(screen, font, "O200200pen", 100, 198, 255, -1);
+void create_button(struct button *b){
+    circle(screen, b->x, b->y, b->r, b->bkgcolor);
+    textout_centre_ex(screen, font, "Open", b->x, b->y, b->bkgcolor, b->textcolor);
 }
 
 void read_sensor(struct tank_t *t){     //sensor
@@ -93,20 +110,27 @@ void update_level(struct tank_t *t){
     t->level = MAXLEVEL - t->sensor;
 }
 
-void check_level(struct tank_t *t){     //unlock the "refiller" if the level il low
+void check_level(struct tank_t *t, void *arg){     //unlock the "refiller" if the level il low
     if (t->level < desired_level){
-        pthread_cond_signal(&t->C_f[t->tnum-1]);
+        pthread_cond_signal(&t->C_f[(intptr_t)arg]);
         }
 }
 
-void check_tap(struct tank_t *t){       //if tap is hold unlock "emptier"
+void check_tap(struct tank_t *t, struct button *b, void *arg){       //if tap is hold unlock "emptier"
+    int x, y;   //mouse coordinates
+    x = mouse_x;
+    y = mouse_y;
+
     if (mouse_b == 1)
-        t->tap = TRUE;
+        if (b->x-b->r < x && x < b->x+b->r && b->y-b->r < y && y < b->y+b->r)
+            t->tap = TRUE;
+        else
+            t->tap = FALSE;       
     else 
         t->tap = FALSE;
 
     if (t->tap && t->level>0)
-        pthread_cond_signal(&t->C_t[t->tnum-1]);
+        pthread_cond_signal(&t->C_t[(intptr_t)arg]);
     }
 
 int get_level(struct tank_t *t){        //measure the level of liquid
@@ -138,6 +162,7 @@ void *th_tap(void *arg){            //thread that manage the tap task
             empty_pixel(t);         //if tank is not empty do it
             update_level(t);
             }
+
         t->tap = FALSE;
         pthread_mutex_unlock(&t->mutex[(intptr_t)arg]);
     }
@@ -163,14 +188,15 @@ void *th_filler(void *arg){         //thread that manage fill task
 
 void *th_tank(void *arg){      //task tank to check the status of tank
     struct tank_t *t = &tank[(intptr_t)arg];
+    struct button *b = &button[(intptr_t)arg];   
     
     while (1){
-        sleep(1/2);
+        usleep(500);
         pthread_mutex_lock(&t->mutex[(intptr_t)arg]);
-        check_tap(t);          //check if the tap is hold
-        check_level(t);        //check liquid level
+        check_tap(t, b, arg);          //check if the tap is hold
+        check_level(t, arg);        //check liquid level
         
-        // printf("livello: %d\t sensor: %d\n", get_level(t), t->sensor);
+        // printf("livello: %d\t sensor: %d\t tank: %ld\n" , get_level(t), t->sensor, (intptr_t)arg);
 
         pthread_mutex_unlock(&t->mutex[(intptr_t)arg]);
     }
@@ -180,7 +206,7 @@ void *th_sensor(void *arg){       //sensor task to evaluate quantity of liquid
     struct tank_t *t = &tank[(intptr_t)arg];
     
     while (1){
-        sleep(1/2);
+        usleep(500);
         pthread_mutex_lock(&t->mutex[(intptr_t)arg]);
         update_level(t);
         pthread_mutex_unlock(&t->mutex[(intptr_t)arg]);
@@ -189,27 +215,30 @@ void *th_sensor(void *arg){       //sensor task to evaluate quantity of liquid
 
 int main(){
     init_allegro();
-    for (int i=0;i<3;i++){
+    for (int i=0;i<N;i++){
         init_tank_t(&tank[i], i+1);      //init della struct
+        init_button(&button[i], &tank[i]);
         create_tank(&tank[i]);
+        create_button(&button[i]);
     }
-    create_button();
 
-    pthread_t tank[2], filler[2], tap[2], sensor[2];
+    pthread_t tank[N-1], filler[N-1], tap[N-1], sensor[N-1];
 
-    for (int i=0;i<3;i++){
+    for (int i=0;i<N;i++){
         pthread_create(&tank[i], NULL, th_tank, (void *)(intptr_t)i);
         pthread_create(&sensor[i], NULL, th_sensor, (void *)(intptr_t)i);
         pthread_create(&filler[i], NULL, th_filler, (void *)(intptr_t)i);
         pthread_create(&tap[i], NULL, th_tap, (void *)(intptr_t)i);
     }
-
-    for (int i=0;i<3;i++){
+    
+    for (int i=0;i<N;i++){
         pthread_join(tank[i], NULL);
+        pthread_join(sensor[i], NULL);
         pthread_join(filler[i], NULL);
         pthread_join(tap[i], NULL);
     }
 
     allegro_exit();
+
     return 0;
 }
