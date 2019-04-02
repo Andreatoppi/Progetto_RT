8f09f44563769c2daa2f355ead25b4c190344e7c
Andreatoppi <and.toppi@gmail.com>
2019-04-02 16:24:42 +0200

    GOOD COMMENT

--- Project_v1.c
+++ Project_v1.c
@@ -8,8 +8,11 @@
 #include <stdbool.h>
 #include <math.h>
 
-#define N           3  //number of tank
-#define XWIN        1000 // window x resolution
+//------------------------------------------------------------------------------
+// GLOBAL CONSTANTS
+//------------------------------------------------------------------------------
+#define N           4  //number of tank
+#define XWIN        850 // window x resolution
 #define YWIN        480 // window y resolution
 #define BKG         0   // background color
 #define X0          250 // x position of sensor
@@ -23,31 +26,37 @@
 #define MINLEVEL    0   // min quantity of liquid in tank
 #define HTANK       300 // tank hight
 #define WTANK       100 // tank width
-#define X1TANK       200 // position of tank
+#define X1TANK      150 // position of tank
 #define Y1TANK      100 // position of tank
 #define R           25  // button radious 
 
 int desired_level = 100;
 
+//------------------------------------------------------------------------------
+// STRUCT DEFINITION. TANK AND BUTTON
+//------------------------------------------------------------------------------
 struct tank_t {     //tank data structure
     pthread_mutex_t mutex[N];
     pthread_cond_t  C_f[N], C_t[N];
     int level;
-    int x1, y1, x2, y2, h, w, color;
-    int sensor, xsensor;
-    int tnum;
-    bool    tap;
+    int x1, y1, x2, y2, h, w, color;    // tank properties
+    int sensor;     // value read from sensor
+    int xsensor;    // x position of sensor
+    int tnum;       // id tank
+    bool    tap;    // tap status
 }tank[N];
 
 struct button {
     int x, y, r;    //circle coordinates
-    int textcolor, bkgcolor;    //text and background color
-    char message;
+    int txtcolor, bkgcolor;     //text and background color
 }button[N];
 
-void init_tank_t(struct tank_t *t, int i){        //tank inizialization
+//------------------------------------------------------------------------------
+// STRUCT INITIALIZATION
+//------------------------------------------------------------------------------
+void init_tank_t(struct tank_t *t, int i){
 
-    pthread_mutex_init(&t->mutex[i-1], NULL);   //mutex and condition variable init
+    pthread_mutex_init(&t->mutex[i-1], NULL);
     pthread_cond_init(&t->C_f[i-1], NULL);
     pthread_cond_init(&t->C_t[i-1], NULL);
     t->level = MINLEVEL;
@@ -68,55 +77,59 @@ void init_button(struct button *b, struct tank_t *t){
     b->x = t->xsensor;
     b->y = t->y1 - 50;
     b->r = R;
-    b->textcolor = BKG;
+    b->txtcolor = BKG;
     b->bkgcolor = WHITE;
-    // b->message = "Open";
 }
-
-//init of allegro
 
 void init_allegro(void){
     allegro_init();
     install_mouse();
+    install_keyboard();
     set_gfx_mode(GFX_AUTODETECT_WINDOWED, XWIN, YWIN, 0, 0);
     clear_to_color(screen, BKG);
     show_mouse(screen);
 }
 
+// Function that print tank on the screen
 void create_tank(struct tank_t *t){
     rectfill(screen, t->x1, t->y1, t->x2, t->y2, t->color);
 }
 
+// Function that print circle button on the screen
 void create_button(struct button *b){
     circle(screen, b->x, b->y, b->r, b->bkgcolor);
-    textout_centre_ex(screen, font, "Open", b->x, b->y, b->bkgcolor, b->textcolor);
+    textout_centre_ex(screen, font, "Open", b->x, b->y, b->bkgcolor, b->txtcolor);
 }
 
-void read_sensor(struct tank_t *t){     //sensor
-    int c = 0;                          // pixel value
-    int y = Y0;                         // sensor coordinates
-    int d = SMIN;                       // min sensor distance
+// Function used by sensor thread to evaluate proximity of liquid
+void read_sensor(struct tank_t *t){
+    int c = 0;      // pixel value
+    int y = Y0;     // y sensor coordinates
+    int d = SMIN;   // min sensor distance
 
     do{
         y = Y0 + d;
         c = getpixel(screen, t->xsensor, y);    //get pixel color
         d = d + SSTEP;
     }while (c == WHITE);
-    t->sensor = d-2; 
+    t->sensor = d-2;    // update sensor value
 }
 
+// Function that read sensor and deduct the level
 void update_level(struct tank_t *t){
     read_sensor(t);
     t->level = MAXLEVEL - t->sensor;
 }
 
-void check_level(struct tank_t *t, void *arg){     //unlock the "refiller" if the level il low
+// Function that unloch the refill if liquid in low
+void check_level(struct tank_t *t, void *arg){
     if (t->level < desired_level){
         pthread_cond_signal(&t->C_f[(intptr_t)arg]);
         }
 }
 
-void check_tap(struct tank_t *t, struct button *b, void *arg){       //if tap is hold unlock "emptier"
+// Function that check mouse status, so the tap status
+void check_tap(struct tank_t *t, struct button *b, void *arg){
     int x, y;   //mouse coordinates
     x = mouse_x;
     y = mouse_y;
@@ -133,22 +146,27 @@ void check_tap(struct tank_t *t, struct button *b, void *arg){       //if tap is
         pthread_cond_signal(&t->C_t[(intptr_t)arg]);
     }
 
-int get_level(struct tank_t *t){        //measure the level of liquid
+int get_level(struct tank_t *t){
     update_level(t);
     return t->level;
 }
 
+// Function that put blue pixel on the screen
 void fill_pixel(struct tank_t *t){
     for (int j=t->x1;j<t->x2+1;j++)
-        putpixel(screen, j, t->y2-t->level, BLUE);  //color line by line in blue
+        putpixel(screen, j, t->y2-t->level, BLUE);
 }
 
+// Function that put white pixel on the screen
 void empty_pixel(struct tank_t *t){
     for (int j=t->x1;j<t->x2+1;j++)
-        putpixel(screen, j, (t->y2-t->level)+1, WHITE); //color line by line in white
+        putpixel(screen, j, (t->y2-t->level)+1, WHITE);
 }
- 
-void *th_tap(void *arg){            //thread that manage the tap task
+
+//------------------------------------------------------------------------------
+// THREAD
+//------------------------------------------------------------------------------
+void *th_tap(void *arg){            //tap sensor
     struct tank_t *t = &tank[(intptr_t)arg];
 
     while (1){
@@ -193,7 +211,7 @@ void *th_tank(void *arg){      //task tank to check the status of tank
     while (1){
         usleep(500);
         pthread_mutex_lock(&t->mutex[(intptr_t)arg]);
-        check_tap(t, b, arg);          //check if the tap is hold
+        check_tap(t, b, arg);       //check if the tap is hold
         check_level(t, arg);        //check liquid level
         
         // printf("livello: %d\t sensor: %d\t tank: %ld\n" , get_level(t), t->sensor, (intptr_t)arg);
@@ -213,10 +231,13 @@ void *th_sensor(void *arg){       //sensor task to evaluate quantity of liquid
     }
 }
 
+//------------------------------------------------------------------------------
+// MAIN FUNCTION
+//------------------------------------------------------------------------------
 int main(){
     init_allegro();
     for (int i=0;i<N;i++){
-        init_tank_t(&tank[i], i+1);      //init della struct
+        init_tank_t(&tank[i], i+1);
         init_button(&button[i], &tank[i]);
         create_tank(&tank[i]);
         create_button(&button[i]);
@@ -229,6 +250,7 @@ int main(){
         pthread_create(&sensor[i], NULL, th_sensor, (void *)(intptr_t)i);
         pthread_create(&filler[i], NULL, th_filler, (void *)(intptr_t)i);
         pthread_create(&tap[i], NULL, th_tap, (void *)(intptr_t)i);
+        usleep(250000);    
     }
     
     for (int i=0;i<N;i++){
