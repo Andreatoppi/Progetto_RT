6e1617340384294f7ee9ef3b93aecc1d0258743a
Andreatoppi <and.toppi@gmail.com>
2019-04-02 17:21:34 +0200

    Modifica nomi file

--- mylib.h
+++ mylib.h
@@ -1,25 +1,24 @@
-#define N           4  //number of tank
-#define XWIN        850 // window x resolution
-#define YWIN        480 // window y resolution
-#define BKG         0   // background color
-#define X0          250 // x position of sensor
-#define Y0          100 // y position of sensor
-#define SMIN        0   // min sensor distance
-#define SMAX        300 // max sensor distance
-#define SSTEP       1   // sensor precision
+#define N           4   // Number of tank
+#define XWIN        850 // Window x resolution
+#define YWIN        480 // Window y resolution
+#define BKG         0   // Background color
+#define X0          250 // X position of sensor
+#define Y0          100 // Y position of sensor
+#define SMIN        0   // Min sensor distance
+#define SMAX        300 // Max sensor distance
+#define SSTEP       1   // Sensor precision
 #define BLUE        11  // VGA code of blue
 #define WHITE       15  // VGA code of white
-#define MAXLEVEL    300 // max quantity of liquid in tank
-#define MINLEVEL    0   // min quantity of liquid in tank
-#define HTANK       300 // tank hight
-#define WTANK       100 // tank width
-#define X1TANK      150 // position of tank
-#define Y1TANK      100 // position of tank
-#define R           25  // button radious 
-#define DESLVL      100 // desired level
+#define MAXLEVEL    300 // Max quantity of liquid in tank
+#define MINLEVEL    0   // Min quantity of liquid in tank
+#define HTANK       300 // Tank hight
+#define WTANK       100 // Tank width
+#define X1TANK      150 // Position of tank
+#define Y1TANK      100 // Position of tank
+#define R           25  // Button radious 
+#define DESLVL      100 // Desired level
 
-
-struct tank_t {     //tank data structure
+struct tank_t {     // Tank data structure
     pthread_mutex_t mutex[N];
     pthread_cond_t  C_f[N], C_t[N];
     int level;
